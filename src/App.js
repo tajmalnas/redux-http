b23fee967b5f19a63afe35c6b0d2ce3c6892a97d
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-04-14 11:06:24 +0530

    added useEffect to redux logic

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
 
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsvisible);
-
+  const cart = useSelector((state)=>state.cart);
+  
+  useEffect(()=>{
+    fetch('https://react-http-7c9e4-default-rtdb.firebaseio.com/cart.json',{
+      method:'PUT',  //OverWrite Existing Data
+      body:JSON.stringify(cart),
+    });
+  },[cart]);
+  
   return (
     <Layout>
       {showCart && <Cart />}
